e2c3027adf00984f5f4b141e852a8800933b2b02
Brian Okinyi <brianokinyi.m@gmail.com>
2025-12-19 16:39:29 +1030

    Minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/brianokinyi.github.io/',
-  publicPath: process.env.NODE_ENV === "production" ? "/brianokinyi.github.io/" : "/",
+  base: '/portfolio/',
 })
